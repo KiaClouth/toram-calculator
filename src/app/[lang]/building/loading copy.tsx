5cd252c0f51adb1b2d9c1c16dc9a9bc9756002cf
KiaClouth <mayunlong16@foxmail.com>
2024-02-23 18:10:37 +0800

    feat: 优化了pc端表现

--- src/app/[lang]/building/loading copy.tsx
+++ src/app/[lang]/building/loading copy.tsx
@@ -7,9 +7,9 @@ export default function Loading() {
     <Link href={"/"}>
     <div className="LoadingPage bg-aeskl fixed flex justify-center items-center left-0 top-0 z-50 h-dvh w-dvw bg-cover bg-center">
       <div className="LoadingMask fixed to-primary-color-0 h-dvh w-dvw bg-gradient-to-t from-primary-color from-5% to-20%"></div>
-      <div className="LoadingState fixed bottom-[2%] left-[4dvw] flex flex-col gap-3">
-        <h1 className="animate-pulse">预计还要加载10年...</h1>
-        <LoadingBox className="w-[92dvw]" />
+      <div className="LoadingState fixed top-[2%] left-[4dvw] lg:left-[10dvw] flex flex-col gap-3">
+        <h1 className="animate-pulse">施工中...</h1>
+        <LoadingBox className="w-[92dvw] lg:w-[80dvw]" />
       </div>
     </div>
     </Link>
