706144a6757b7b75a5ba6ef23288fc857d1b7039
kiaclouth <mayunlong16@foxmail.com>
2024-02-25 23:11:21 +0800

    feat: 尝试使用zod验证

--- src/app/[lang]/pet/client.tsx
+++ src/app/[lang]/pet/client.tsx
@@ -1,29 +1,75 @@
 "use client";
 import React from "react";
-import { useForm } from "@tanstack/react-form";
+import { type FieldApi, useForm } from "@tanstack/react-form";
 import { type getDictionary } from "~/app/get-dictionary";
-import { type Monster } from "@prisma/client";
+import { type $Enums } from "@prisma/client";
 import { type Session } from "next-auth";
-import { useState } from "react";
+import { type Monster, MonsterSchema } from "prisma/generated/zod";
+import { z } from "zod";
+import { zodValidator } from "@tanstack/zod-form-adapter";
 
 export default function PetPageClient(props: {
   dictionary: ReturnType<typeof getDictionary>;
   session: Session | null;
 }) {
   // 怪物数据的初始值
+  const defaultMonster: Monster = {
+    id: "",
+    updatedAt: new Date(),
+    updatedById: "",
+    state: "PRIVATE",
+    name: "",
+    type: "COMMON_BOSS",
+    baseLv: 0,
+    experience: 0,
+    address: "",
+    element: "NO_ELEMENT",
+    radius: 1,
+    maxhp: 0,
+    physicalDefense: 0,
+    physicalResistance: 0,
+    magicalDefense: 0,
+    magicalResistance: 0,
+    criticalResistance: 0,
+    avoidance: 0,
+    dodge: 0,
+    block: 0,
+    normalAttackResistanceModifier: 0,
+    physicalAttackResistanceModifier: 0,
+    magicalAttackResistanceModifier: 0,
+    difficultyOfTank: 0,
+    difficultyOfMelee: 0,
+    difficultyOfRanged: 0,
+    possibilityOfRunningAround: 0,
+    specialBehavior: "",
+    createdByUserId: "",
+  };
+
+  MonsterSchema.parse(defaultMonster)
   const { dictionary, session } = props;
+  function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
+    return (
+      <>
+        {field.state.meta.touchedErrors ? (
+          <em>{field.state.meta.touchedErrors}</em>
+        ) : null}
+        {field.state.meta.isValidating ? "Validating..." : null}
+      </>
+    );
+  }
   const form = useForm({
-    defaultValues: {
-      fullName: "",
-      age: 0,
-    },
+    defaultValues: defaultMonster,
     onSubmit: async ({ value }) => {
       // Do something with form data
       console.log(value);
     },
+    // Add a validator to support Zod usage in Form and Field
+    validatorAdapter: zodValidator,
   });
+
   return (
-    <React.Fragment>
+    <div>
+      <h1>Zod Form Example</h1>
       <form.Provider>
         <form
           onSubmit={(e) => {
@@ -33,46 +79,56 @@ export default function PetPageClient(props: {
           }}
         >
           <div>
-            <form.Field name="fullName">
-              {(field) => (
-                <input
-                  name={field.name}
-                  value={field.state.value}
-                  onBlur={field.handleBlur}
-                  onChange={(e) => field.handleChange(e.target.value)}
-                />
-              )}
-            </form.Field>
+            {/* A type-safe field component*/}
             <form.Field
-              name="age"
+              name="state"
               validators={{
-                onBlur: ({ value }) =>
-                  value < 13 ? "You must be 13 to make an account" : undefined,
+                onChange: MonsterSchema,
+                onChangeAsyncDebounceMs: 500,
+                onChangeAsync: z.string().refine(
+                  async (value) => {
+                    await new Promise((resolve) => setTimeout(resolve, 1000));
+                    return !value.includes("error");
+                  },
+                  {
+                    message: "No 'error' allowed in first name",
+                  },
+                ),
               }}
             >
-              {(field) => (
-                <>
-                  <label htmlFor={field.name}>Age:</label>
-                  <input
-                    id={field.name}
-                    name={field.name}
-                    value={field.state.value}
-                    type="number"
-                    // Listen to the onBlur event on the field
-                    onBlur={field.handleBlur}
-                    // We always need to implement onChange, so that TanStack Form receives the changes
-                    onChange={(e) => field.handleChange(e.target.valueAsNumber)}
-                  />
-                  {field.state.meta.errors ? (
-                    <em role="alert">{field.state.meta.errors.join(", ")}</em>
-                  ) : null}
-                </>
-              )}
+              {(field) => {
+                // Avoid hasty abstractions. Render props are great!
+                return (
+                  <>
+                    <label htmlFor={field.name}>First Name:</label>
+                    <input
+                      id={field.name}
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) =>
+                        field.handleChange(
+                          e.target.value as keyof typeof $Enums.State,
+                        )
+                      }
+                    />
+                    <FieldInfo field={field} />
+                  </>
+                );
+              }}
             </form.Field>
           </div>
-          <button type="submit">Submit</button>
+          <form.Subscribe
+            selector={(state) => [state.canSubmit, state.isSubmitting]}
+          >
+            {([canSubmit, isSubmitting]) => (
+              <button type="submit" disabled={!canSubmit}>
+                {isSubmitting ? "..." : "Submit"}
+              </button>
+            )}
+          </form.Subscribe>
         </form>
       </form.Provider>
-    </React.Fragment>
+    </div>
   );
 }
