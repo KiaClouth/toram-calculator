ea706020d905fb6194fbf653d40f5806143e341b
KiaClouth <mayunlong16@foxmail.com>
2024-03-06 18:39:51 +0800

    feat: 为页面添加了loadingPage

--- src/app/[lang]/_components/loadingBox.tsx
+++ src/app/[lang]/_components/loadingBox.tsx
@@ -3,10 +3,10 @@ import * as React from "react";
 export default function LoadingBox(props: React.HTMLProps<HTMLDivElement>) {
   return (
     <div {...props} className={`Loading relative overflow-x-hidden ${{ ...props }.className}`}>
-      <div className="line z-0 h-1 w-full bg-accent-color rounded-full"></div>
-      <div className="break dot1 absolute top-0 z-10 h-1 w-1 bg-primary-color"></div>
-      <div className="break dot2 absolute top-0 z-10 h-1 w-1 bg-primary-color"></div>
-      <div className="break dot3 absolute top-0 z-10 h-1 w-1 bg-primary-color"></div>
+      <div className="line z-0 h-0.5 w-full bg-accent-color rounded-full"></div>
+      <div className="break dot1 absolute top-0 z-10 h-0.5 w-1 bg-primary-color"></div>
+      <div className="break dot2 absolute top-0 z-10 h-0.5 w-1 bg-primary-color"></div>
+      <div className="break dot3 absolute top-0 z-10 h-0.5 w-1 bg-primary-color"></div>
     </div>
   );
 }
---
+++ src/app/[lang]/pet/loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoadingBox from "../_components/loadingBox";
+
+export default function PetLoading() {
+  return (
+    <div className="LoadingModule w-full h-full flex justify-center items-center backdrop-blur">
+      <LoadingBox className=" w-2/3" />
+    </div>
+  );
+}
