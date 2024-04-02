fe8e77086cf9b547a2e887cc17c4d38cf4ad0980
KiaClouth <mayunlong16@foxmail.com>
2024-04-02 14:57:23 +0800

    feat: 类型修改器更新

--- src/app/dictionaries/type.d.ts
+++ src/app/dictionaries/type.d.ts
@@ -1,13 +1,11 @@
 import type { $Enums, Monster } from "@prisma/client";
 
 // 为了方便编辑器自动补全，这个方法可以将数据库模型的值类型转换为字符串
-type ConvertToAllString<T> = T extends object | number
+type ConvertToAllString<T> = T extends object
   ? {
       [K in keyof T]: T[K] extends Date | Date[]
         ? string
-        : T[K] extends Array
-          ? string
-          : ConvertToAllString<T[K]>;
+        : ConvertToAllString<T[K]>;
     }
   : string;
 
