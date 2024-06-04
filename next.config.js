806ec410954ac38fe054025928ee90ddcd6a9c13
KiaClouth <mayunlong16@foxmail.com>
2024-06-04 18:23:12 +0800

    feat: 关闭sw

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ import withSerwistInit from "@serwist/next";
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
-  disable: false,
+  disable: true,
 });
 
 export default withSerwist({
