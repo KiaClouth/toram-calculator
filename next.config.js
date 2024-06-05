0e74713dd858c02baa63d67c2bffbb7a1b76ff69
KiaClouth <mayunlong16@foxmail.com>
2024-06-05 12:15:23 +0800

    feat: emm

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ export default withSerwist({
   reactStrictMode: true, // Enable React strict mode for improved error handling
   swcMinify: true, // Enable SWC minification for improved performance
   compiler: {
-    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+    // removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
   images: {
     remotePatterns: [
